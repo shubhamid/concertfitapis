7d6f777f04bd6656b62e5190123f21dbd7d2b8e7
shubhamdas <shubhamid@gmail.com>
2019-11-18 01:56:47 -0500

    ticketmaster api

---
+++ concertfitapis/concertfitapis/Pages/TicketmasterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace concertfitapis.Pages
+{
+    [Route("api/ticketmaster")]
+    public class TicketmasterController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public TicketMasterResponse.TopLevel Get()
+        {
+            using (StreamReader r = new StreamReader("ticketmaster.json"))
+            {
+                string json = r.ReadToEnd();
+                TicketMasterResponse.TopLevel ticketMasterResponseTopLevel = TicketMasterResponse.TopLevel.FromJson(json);
+                /*TicketMasterResponse.TopLevel ticketMasterResponseTopLevel = JsonConvert.DeserializeObject<TicketMasterResponse.TopLevel>(json);*/
+                return ticketMasterResponseTopLevel;
+            }
+        }
+    }
+}
